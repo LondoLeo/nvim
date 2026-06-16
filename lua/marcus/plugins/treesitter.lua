ec19d54cb079534dcfd9098c17c6ba61a282cd9f
Marcus Faulstich <marcus.faulstich@tu-ilmenau.de>
2026-06-16 10:24:11 +0200

    fixed treesitter

--- lua/marcus/plugins/treesitter.lua
+++ lua/marcus/plugins/treesitter.lua
@@ -1,21 +1,18 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
-        branch = 'master',
+        branch = 'main',
         lazy = false,
         build = ":TSUpdate",
         config = function()
-            require('nvim-treesitter.configs').setup{
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false,
-                },
-                auto_install = true,
-                indent = {
-                    enable = true
-                },
-                ensure_installed = { "c", "cpp", "html", "go", "php", "javascript", "python", "rust", "swift" }
-            }
+            vim.api.nvim_create_autocmd('FileType', { 
+                    callback = function() 
+                    -- Enable treesitter highlighting and disable regex syntax
+                    pcall(vim.treesitter.start) 
+                    -- Enable treesitter-based indentation
+                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()" 
+                    end, 
+                    }) 
         end
     }
 }
